d45fdd1f2cdf2243ff9ff85c2d4bf9d4cbd5bdfb
Ramadhin Zaid <61625805+ramadhinzaid@users.noreply.github.com>
2023-08-24 15:13:46 +0700

    add dart code

---
+++ main.dart
@@ -0,0 +1,123 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  //Event Channel creation
+  EventChannel stream = const EventChannel('poc.deeplink.flutter.dev/events');
+  //Method channel creation
+  MethodChannel platform =
+      const MethodChannel('poc.deeplink.flutter.dev/channel');
+
+  final StreamController<String> _stateController = StreamController();
+
+  Stream<String> get state => _stateController.stream;
+
+  Sink<String> get stateSink => _stateController.sink;
+
+  @override
+  void initState() {
+    startUri().then(_onRedirected);
+    //Checking broadcast stream, if deep link was clicked in opened appication
+    stream.receiveBroadcastStream().listen((d) => _onRedirected(d));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stateController.close();
+  }
+
+  _onRedirected(String? uri) {
+    stateSink.add(uri ?? "");
+  }
+
+  Future<String?> startUri() async {
+    try {
+      return platform.invokeMethod('initialLink');
+    } on PlatformException catch (e) {
+      return "Failed to Invoke: '${e.message}'.";
+    }
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            StreamBuilder<String>(
+                stream: state,
+                builder: (ctx, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: Text('No deep link was used  '));
+                  } else {
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text('Redirected: ${snapshot.data}'),
+                      ),
+                    );
+                  }
+                })
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
